e51c8cb4e32424a21fb32172869a1f7506219aea
marchamil <74551298+marchamil@users.noreply.github.com>
2020-11-17 21:32:59 +0100

    Completed tutorial

--- fruitseller/flow/interaction.kt
+++ fruitseller/flow/interaction.kt
@@ -22,8 +22,8 @@ val Start : State = state(Interaction) {
     //If user asks for fruit directly
     onResponse<BuyFruit> {
         val fruits = it.intent.fruits
-        furhat.say("${fruits.text}, what a lovely choice!")
-        fruits.list.forEach {
+        furhat.say("${fruits!!.text}, what a lovely choice!")
+        fruits!!.list.forEach {
             users.current.order.fruits.list.add(it)
         }
     }
@@ -34,14 +34,38 @@ val Start : State = state(Interaction) {
     }
 }
 
-val TakingOrder = state {
+// Parent state for eliminating repetitive coding
+val Options = state(Interaction) {
+    onResponse<BuyFruit> {
+        val fruits = it.intent.fruits
+        if (fruits != null) {
+            goto(OrderReceived(fruits))
+        } else {
+            propagate()
+        }
+    }
+
+    onResponse<RequestOptions> {
+        furhat.say("We have ${Fruit().optionsToText()}")
+        furhat.ask("Do you want some?")
+    }
+
+    onResponse<Yes> {
+        random(
+                { furhat.ask("What kind of fruit do you want?") },
+                { furhat.ask("What type of fruit?") }
+        )
+    }
+}
+
+val TakingOrder = state(parent = Options) {
     onEntry {
         random(
                 { furhat.ask("I hope you are well, how about some fruits?") },
                 { furhat.ask("I hope you are well, do you want some fruits?") }
         )
     }
-
+    /*
     onReentry {
         random(
                 { furhat.ask("What kind of fruit do you want?") },
@@ -55,12 +79,12 @@ val TakingOrder = state {
                 { furhat.ask("What type of fruit?") }
         )
     }
-
+    */
     onResponse<No> {
         furhat.say("Okay, that's a shame. Have a splendid day!")
         goto(Idle)
     }
-
+    /*
     onResponse<RequestOptions> {
         furhat.say("We have ${Fruit().optionsToText()}")
         furhat.ask("Do you want some?")
@@ -68,11 +92,56 @@ val TakingOrder = state {
 
     onResponse<BuyFruit> {
         val fruits = it.intent.fruits
+        if(fruites != null) {
+            goto(OrderReceived(fruits))
+        } else {
+            propagate()
+        }
+       /*
+        * Old version of BuyFruit
         furhat.say("${fruits.text}, what a lovely choice!")
         fruits.list.forEach {
             users.current.order.fruits.list.add(it)
+
         }
+        */
+
     }
+    */
 }
 
+fun OrderReceived(fruits: FruitList) : State = state(Options) {
+    onEntry {
+        furhat.say("${fruits.text}, what a lovely choice!")
+        fruits.list.forEach{
+            users.current.order.fruits.list.add(it)
+        }
+        furhat.ask("Anything else?")
+    }
+
+    onReentry {
+        furhat.ask("Did you want something else?")
+    }
+    /*
+    onResponse<BuyFruit> {
+        val fruits = it.intent.fruits
+        if(fruits != null){
+            goto(OrderReceived(fruits))
+        } else {
+            propagate()
+        }
+    }
+
+    onResponse<Yes> {
+        random(
+                {furhat.ask("What kind of fruit do you want?")},
+                {furhat.ask("What type of fruit?")}
+        )
+    }
+    */
+    onResponse<No> {
+        furhat.say("Okay, here is your order of ${users.current.order.fruits}. Have a great day!")
+        goto(Idle)
+    }
+}
 
